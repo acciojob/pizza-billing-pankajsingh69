8999dc52077333f57188b07f9e38a9b36181f867
pankaj <adhikarisinghpankaj231@gmail.com>
2023-02-24 21:01:54 +0530

    first commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,31 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int toppings;
+
+    boolean isExtraCheeseAdded;
+    boolean isExtraToppingAdded;
+    boolean isBillCreated;
+    boolean isTakeAway;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price = 300;
+            this.toppings = 70;
+        }
+        else{
+            this.price = 400;
+            this.toppings = 120;
+        }
+        this.isExtraCheeseAdded = false;
+        this.isExtraToppingAdded = false;
+        this.isBillCreated = false;
+        this.isTakeAway = false;
+
+        this.bill = "Base Price Of The Pizza: " +this.price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +39,44 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheeseAdded){
+            this.price += 80;
+            this.isExtraCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraToppingAdded){
+            this.price += this.toppings;
+            this.isExtraToppingAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAway){
+            this.price += 20;
+            this.isTakeAway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(!isBillCreated){
+            if(isExtraCheeseAdded){
+                this.bill += "Extra Cheese Added: 80"+"\n";
+            }
+            if(isExtraToppingAdded){
+                this.bill += "Extra Toppings Added: "+this.toppings+"\n";
+            }
+            if(isTakeAway){
+                this.bill += "Paperbag Added: 20"+"\n";
+            }
+            this.bill += "Total Price: "+this.price+"\n";
+            this.isBillCreated = true;
+            return this.bill;
+        }else
+            return this.bill;
     }
 }
